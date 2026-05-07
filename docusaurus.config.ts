13486b8aa32db155579ec023b65af0f2baf89385
MB <84203045+MonadoArt@users.noreply.github.com>
2026-05-07 17:07:25 +0200

    more link god damn fixes damnit

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://monadoart.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/vrchat-avatar-wiki-guide/docs/intro',
+  baseUrl: '/vrchat-avatar-wiki-guide/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
